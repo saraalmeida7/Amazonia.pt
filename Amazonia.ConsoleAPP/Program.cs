a3695a223dc9442dea567278e25fdb100512523b
SaraAlmeida <ss.almeida@campus.fct.unl.pt>
2021-10-22 20:43:12 +0100

    TPC - repositorio livro

--- Amazonia.ConsoleAPP/Program.cs
+++ Amazonia.ConsoleAPP/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Amazonia.DAL;
 using Amazonia.DAL.Repositorios;
+using Amazonia.DAL.Entidades;
 
 namespace Amazonia.ConsoleAPP
 {
@@ -10,31 +11,51 @@ namespace Amazonia.ConsoleAPP
         {
             Console.WriteLine("Consulta do Database");
 
-            var repo = new RepositorioCliente();
-            //var listaClientes = repo.ObterTodos();
+            var repoLivro = new RepositorioLivro();
+            var todosLivros = repoLivro.ObterTodos();
 
-            var listaClientes = repo.ObterTodosQueComecemPor("J");
-
-            foreach (var cliente in listaClientes)
-            {
-                System.Console.WriteLine(cliente);
-            }
-
-            var listaClientesAdultos = repo.ObterTodosQueTenhamPeloMenos18Anos();
-            foreach(var item in listaClientesAdultos)
-            {
+            foreach(var item in todosLivros){
                 System.Console.WriteLine(item);
             }
 
-             var listagemTotal = repo.ObterTodos();
+            repoLivro.Criar(new LivroImpresso{Nome = "contos Infantis"});
 
-            var joao = repo.ObterPorNome("Joao");
-            System.Console.WriteLine(joao);
-            System.Console.WriteLine($"Database contem: {listagemTotal.Count}");
+            foreach(var item in todosLivros){
+                System.Console.WriteLine(item);
+            }
 
-            repo.Apagar(joao);
+            repoLivro.Apagar(todosLivros.Find(x => x.Nome == "historias"));
+            repoLivro.Atualizar("Terror", "MuitoTerror");
 
-            System.Console.WriteLine($"Database contem: {listagemTotal.Count}");
+            foreach(var item in todosLivros){
+                System.Console.WriteLine(item);
+            }
+
+            // var repo = new RepositorioCliente();
+            // //var listaClientes = repo.ObterTodos();
+
+            // var listaClientes = repo.ObterTodosQueComecemPor("J");
+
+            // foreach (var cliente in listaClientes)
+            // {
+            //     System.Console.WriteLine(cliente);
+            // }
+
+            // var listaClientesAdultos = repo.ObterTodosQueTenhamPeloMenos18Anos();
+            // foreach(var item in listaClientesAdultos)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+
+            //  var listagemTotal = repo.ObterTodos();
+
+            // var joao = repo.ObterPorNome("Joao");
+            // System.Console.WriteLine(joao);
+            // System.Console.WriteLine($"Database contem: {listagemTotal.Count}");
+
+            // repo.Apagar(joao);
+
+            // System.Console.WriteLine($"Database contem: {listagemTotal.Count}");
 
             // var clienteNovo = repo.Atualizar(joao.Nome, )
 
---
+++ Amazonia.DAL/Repositorios/RepositorioLivro.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using Amazonia.DAL.Entidades;
+using System.Linq;
+
+namespace Amazonia.DAL.Repositorios
+{
+    public class RepositorioLivro : IRepositorio<Livro>
+    {
+
+            public List<Livro> ListaLivros { get; set; }
+
+            public RepositorioLivro(){
+                ListaLivros = new List<Livro>();
+
+                var livroHistorias = new LivroDigital{Nome = "historias"};
+                var livroTerror = new LivroDigital{Nome = "Terror"};
+
+                ListaLivros.Add(livroHistorias);
+                ListaLivros.Add(livroTerror);
+
+            //   var joao = new Cliente();
+            // joao.Nome = "Joao";
+            // joao.DataNascimento = new DateTime(1984,05,29);
+
+            // var maria = new Cliente{Nome = "Maria", 
+            //     DataNascimento = new DateTime(1950,01,01)};
+            // var marta = new Cliente{Nome = "Marta", 
+            //     DataNascimento = new DateTime(2021,01,02)};
+        
+        
+            // ListaClientes.Add(joao);
+            // ListaClientes.Add(maria);
+            //ListaClientes.Add(marta);
+        }
+
+        public void Apagar(Livro obj)
+        {
+            ListaLivros.Remove(obj);
+        }
+
+        public Livro Atualizar(string nomeAntigo, string nomeNovo)
+        {
+           var livroTemporario = ListaLivros.
+                                    Where(x => x.Nome == nomeAntigo).FirstOrDefault();
+           livroTemporario.Nome = nomeNovo;
+                
+           return livroTemporario;
+        }
+
+        public void Criar(Livro obj)
+        {
+            ListaLivros.Add(obj);
+        }
+
+        public Livro ObterPorNome(string nome)
+        {
+            var resultado = ListaLivros.
+                             Where(x => x.Nome == nome).FirstOrDefault();
+            
+            return resultado;
+
+        }
+
+        public List<Livro> ObterTodos()
+        {
+            return ListaLivros;
+        }
+    }
+
+}
