9abe5403788d6af04490b44e3c9fc5e98b06b3ba
SaraAlmeida <ss.almeida@campus.fct.unl.pt>
2021-10-26 21:47:36 +0100

    Adicionados testes automatizados

---
+++ Amazonia.BLL.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Amazonia.BLL.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
